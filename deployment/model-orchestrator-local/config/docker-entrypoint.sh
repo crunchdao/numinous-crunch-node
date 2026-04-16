ccbd5dbfef409c3c11d9ad95ab4ce8070c501c05
Caceresenzo <caceresenzo1502@gmail.com>
2026-04-16 18:09:45 +0000

    fix(local): change model orchestrator setup

--- deployment/model-orchestrator-local/config/docker-entrypoint.sh
+++ deployment/model-orchestrator-local/config/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-#if [ ! -f /app/data/orchestrator.dev.db ]; then
-#  echo "Downloading the notebook for local example execution. You can modify it later via the UI at http://localhost:3000/models."
-#  model-orchestrator dev \
-#    --configuration-file /app/config/orchestrator.dev.yml \
-#    import https://github.com/crunchdao/numinous/blob/master/examples/exampletracker.ipynb \
-#    --import-choice 1 \
-#    --import-name numinous-benchmarktracker
-#fi
+if [ ! -f /app/data/orchestrator.dev.db ]; then
+ echo "Downloading the notebook for local example execution. You can modify it later via the UI at http://localhost:3000/models."
+ model-orchestrator dev \
+   --configuration-file /app/config/orchestrator.dev.yml \
+   import https://github.com/crunchdao/crunch-numinous/blob/main/numinous/examples/quickstart.ipynb \
+   --import-choice 1 \
+   --import-name numinous-benchmarktracker
+fi
 
 exec "$@"   # runs the real CMD
